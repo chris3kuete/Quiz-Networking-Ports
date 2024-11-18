42f70537f4dc280597a38f28274df952cf0fc666
chris3kuete <chriskuete@yahoo.fr>
2024-11-18 11:30:08 -0800

    1

---
+++ js/questions.js
@@ -0,0 +1,245 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the port number for HTTP ?",
+        answer: "80",
+        options: [
+            "80",
+            "81",
+            "82",
+            "79"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the port number for HTTPS ?",
+        answer: "443",
+        options: [
+            "240",
+            "340",
+            "443",
+            "540"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the port number for RDP ?",
+        answer: "3389",
+        options: [
+            "3389",
+            "3380",
+            "2800",
+            "3500"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the port number for DNS?",
+        answer: "53",
+        options: [
+            "50",
+            "51",
+            "52",
+            "53"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the port number for SSH ?",
+        answer: "22",
+        options: [
+            "22",
+            "21",
+            "20",
+            "23"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is the port number for SMB ?",
+        answer: "445",
+        options: [
+            "345",
+            "441",
+            "445",
+            "444"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the port number for TELNET ?",
+        answer: "23",
+        options: [
+            "27",
+            "26",
+            "23",
+            "28"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the port number for POP3 ?",
+        answer: "110",
+        options: [
+            "110",
+            "98",
+            "80",
+            "95"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the port number for IMAP ?",
+        answer: "143",
+        options: [
+            "120",
+            "143",
+            "122",
+            "150"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the port number for SMTP",
+        answer: "25",
+        options: [
+            "31",
+            "30",
+            "25",
+            "32"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What is the port number for FTP",
+        answer: "21",
+        options: [
+            "21",
+            "35",
+            "28",
+            "42"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What is the port number for SFTP",
+        answer: "22",
+        options: [
+            "35",
+            "34",
+            "22",
+            "39"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What is the port number for DHCP",
+        answer: "68",
+        options: [
+            "68",
+            "80",
+            "85",
+            "90"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What is the port number for TFTP",
+        answer: "69",
+        options: [
+            "71",
+            "69",
+            "75",
+            "82"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What is the port number for NTP",
+        answer: "123",
+        options: [
+            "123",
+            "100",
+            "125",
+            "132"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What is the port number for IMAP4",
+        answer: "143",
+        options: [
+            "131",
+            "143",
+            "215",
+            "321"
+        ]
+    },
+    {
+        numb: 17,
+        question: "What is the port number for SNMP",
+        answer: "161",
+        options: [
+            "331",
+            "330",
+            "161",
+            "302"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What is the port number for LDAP",
+        answer: "389",
+        options: [
+            "311",
+            "3023",
+            "235",
+            "389"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What is the port number for LDAPS",
+        answer: "636",
+        options: [
+            "319",
+            "303",
+            "653",
+            "636"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What is the port number for H.323",
+        answer: "1720",
+        options: [
+            "3132",
+            "3034",
+            "1720",
+            "3233"
+        ]
+    },
+    {
+        numb: 21,
+        question: "What is the port number for SIP",
+        answer: "5060",
+        options: [
+            "5063",
+            "5060",
+            "4023",
+            "5054"
+        ]
+    },
+    {
+        numb: 22,
+        question: "What is the port number for FTP-DATA",
+        answer: "20",
+        options: [
+            "24",
+            "26",
+            "20",
+            "12"
+        ]
+    },
+
+]
